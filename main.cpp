634e76bff4597eacd705df51e158f6474b5299ad
ChainofChaos <stephenjelek@yahoo.com>
2022-07-12 21:42:29 +0700

    Storing list of countries

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <map>
 
 std::vector<std::string> splitString(const std::string &text, char delim) {
     std::vector<std::string> splits;
@@ -18,10 +19,12 @@ std::vector<std::string> splitString(const std::string &text, char delim) {
 int main() {
     std::ifstream fastaFile(R"(D:\source\Python\Gisaid\spikeprot0628\spikeprot0628.fasta)");
     std::string textFile;
+    std::map<std::string, std::map<std::string, int>> count;
+    std::vector<std::string> countries;
     std::filesystem::create_directory("Countries");
     if (fastaFile.is_open()) {
         bool isHuman = true;
-        std::string header, sequence, country;
+        std::string header, sequence, country, date;
         while (std::getline(fastaFile, textFile)) {
             if (textFile[0] == '>') {
                 std::vector<std::string> splittedString = splitString(textFile, '|');
@@ -30,18 +33,34 @@ int main() {
                     continue;
                 }
                 isHuman = true;
+                date = splittedString[2];
                 country = splitString(splittedString[1], '/')[1];
+                if (std::find(countries.begin(), countries.end(), country) != countries.end()) {
+                    countries.push_back(country);
+                }
                 header = textFile;
             } else {
                 if (!isHuman) {
                     continue;
                 }
                 sequence = textFile;
+                count[date.substr(0, date.find_last_of('-'))][country] += 1;
+//                for (auto const& [key,val] : count){
+//                    for (auto const& [country, counts] : val){
+//                        std::cout << "Key : " << key << " Country: " << country << " Counts : " << counts << std::endl;
+//                    }
+//                }
                 std::ofstream outCountryFastaFile("Countries/" + country + ".fasta", std::ios_base::app);
                 outCountryFastaFile << header << '\n';
                 outCountryFastaFile << sequence << '\n';
             }
         }
     }
+    std::ofstream outCount("DateCount.csv");
+    outCount << "date,count\n";
+
+//    for (auto const &[key, val]: count) {
+//        outCount << key << ',' << val << '\n';
+//    }
     return 0;
 }
